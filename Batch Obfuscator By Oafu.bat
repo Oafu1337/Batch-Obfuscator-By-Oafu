13de8e7516c4baa7fc83085b9ae3f4ddc7460748
Oafu <65862850+Oafu1337@users.noreply.github.com>
2020-05-26 22:15:39 +0200

    Add files via upload

---
+++ Batch Obfuscator By Oafu.bat
@@ -0,0 +1,152 @@
+ÿþ&cls
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+:Batch Obfuscator By Oafu:
+
+
+
+
+
+
+
+:Dont Skid n00b:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+@echo off
+
+cls@start app  
+:Lance le fichier .js qui va obfusquer votre fichier:
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if "%~1"=="" exit /b
+	
+if /i "%~x1" neq ".bat" if /i "%~x1" neq ".cmd" exit /b
+	
+for /f %%i in ("certutil.exe") do if not exist "%%~$path:i" (
+	
+  echo CertUtil.exe not found.
+	
+  pause
+	
+  exit /b
+	
+)
+	
+>"temp.~b64" echo(//4mY2xzDQo=
+	
+certutil.exe -f -decode "temp.~b64" "%~n1___%~x1"
+	
+del "temp.~b64"
+	
+copy "%~n1___%~x1" /b + "%~1" /b
